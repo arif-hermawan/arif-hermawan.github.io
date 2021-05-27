c6a474acb3d18a18caf87130c10444a017a5c428
Arif Hermawan <arif-hermawan@hotmail.com>
2021-05-27 22:00:55 +0800

    menambah project privataja

---
+++ R-privataja/material/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () { 
+    $(document).click(function () {
+       // if($(".navbar-collapse").hasClass("in")){
+         $('.navbar-collapse').collapse('hide');
+       // }
+    });
+  });
